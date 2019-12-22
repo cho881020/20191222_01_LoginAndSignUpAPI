8386ec5cc90d8fa206f77949678345893c7d74c4
조경진 <kj_cho@idealidea.co.kr>
2019-12-22 11:20:07 +0900

    회원가입 화면초안 작성

---
+++ app/src/main/java/com/tjeit/a20191222_01_loginandsignupapi/SignUpActivity.kt
@@ -0,0 +1,24 @@
+package com.tjeit.a20191222_01_loginandsignupapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+        setupEvents()
+        setValues()
+    }
+
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
