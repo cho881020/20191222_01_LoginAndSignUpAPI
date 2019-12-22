09a8ea8fc63e76fb50547c93b18f175e170e2356
조경진 <kj_cho@idealidea.co.kr>
2019-12-22 16:45:41 +0900

    등록 버튼 눌리면, 뭐라고 적었는지 추출

--- app/src/main/java/com/tjeit/a20191222_01_loginandsignupapi/EditBlackListActivity.kt
+++ app/src/main/java/com/tjeit/a20191222_01_loginandsignupapi/EditBlackListActivity.kt
@@ -2,6 +2,7 @@ package com.tjeit.a20191222_01_loginandsignupapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_black_list.*
 
 class EditBlackListActivity : BaseActivity() {
 
@@ -13,6 +14,17 @@ class EditBlackListActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        postBtn.setOnClickListener {
+
+//            사용자가 입력한 값을 저장
+            val title = titleEdt.text.toString()
+            val phone = phoneNumEdt.text.toString()
+            val content = contentEdt.text.toString()
+
+//            서버에 게시글 등록을 요청하는 기능을 추가해야함
+
+        }
 
     }
 
