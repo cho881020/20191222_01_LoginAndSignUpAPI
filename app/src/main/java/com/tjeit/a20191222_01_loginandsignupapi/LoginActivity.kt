9d89214e0225843560da618b1bfab46c14170f8b
조경진 <kj_cho@idealidea.co.kr>
2019-12-22 11:09:55 +0900

    Initial commit

---
+++ app/src/main/java/com/tjeit/a20191222_01_loginandsignupapi/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.tjeit.a20191222_01_loginandsignupapi
+
+import android.os.Bundle
+
+class LoginActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
