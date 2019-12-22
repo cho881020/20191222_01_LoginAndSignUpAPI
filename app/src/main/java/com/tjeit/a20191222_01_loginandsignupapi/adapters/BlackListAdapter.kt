bbb57aa6670656df174884fc3e76d7d743d6275e
조경진 <kj_cho@idealidea.co.kr>
2019-12-22 15:52:36 +0900

    어댑터 기초 작업 작성

---
+++ app/src/main/java/com/tjeit/a20191222_01_loginandsignupapi/adapters/BlackListAdapter.kt
@@ -0,0 +1,42 @@
+package com.tjeit.a20191222_01_loginandsignupapi.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.tjeit.a20191222_01_loginandsignupapi.R
+import com.tjeit.a20191222_01_loginandsignupapi.datas.BlackListData
+
+class BlackListAdapter (context: Context, resId: Int, list:ArrayList<BlackListData>)
+    : ArrayAdapter<BlackListData> (context, resId, list) {
+
+    val mContext = context
+    val mList = list
+    val inf = LayoutInflater.from(mContext)
+
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+
+        tempRow?.let {
+
+        }.let {
+            tempRow = inf.inflate(R.layout.black_list_item, null)
+        }
+
+        val row = tempRow!!
+
+
+        return row
+    }
+
+
+}
+
+
+
+
+
+
+
