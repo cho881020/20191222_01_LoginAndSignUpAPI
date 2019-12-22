1dc585850ebeca25e2e30099dbc84652ef8d1337
조경진 <kj_cho@idealidea.co.kr>
2019-12-22 17:02:41 +0900

    실제 API 호출 후 로그 확인

--- app/src/main/java/com/tjeit/a20191222_01_loginandsignupapi/EditBlackListActivity.kt
+++ app/src/main/java/com/tjeit/a20191222_01_loginandsignupapi/EditBlackListActivity.kt
@@ -2,8 +2,10 @@ package com.tjeit.a20191222_01_loginandsignupapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.tjeit.a20191222_01_loginandsignupapi.utils.ConnectServer
 import kotlinx.android.synthetic.main.activity_edit_black_list.*
+import org.json.JSONObject
 
 class EditBlackListActivity : BaseActivity() {
 
@@ -25,6 +27,14 @@ class EditBlackListActivity : BaseActivity() {
 
 //            서버에 게시글 등록을 요청하는 기능을 추가해야함
 
+//            실제로 서버에 게시글 등록 API를 호출하는 코드
+            ConnectServer.postRequestBlackList(mContext, title, phone, content, object : ConnectServer.JsonResponseHandler {
+                override fun onResponse(json: JSONObject) {
+//                    서버의 응답을 확인
+                    Log.d("게시글등록응답", json.toString())
+                }
+
+            })
 
 
         }
