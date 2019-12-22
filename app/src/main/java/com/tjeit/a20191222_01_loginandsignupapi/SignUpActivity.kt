14e027512d7b5f0e072e0121d08e3f8ad549abad
조경진 <kj_cho@idealidea.co.kr>
2019-12-22 12:20:04 +0900

    API 호출 성공  - 응답처리 필요

--- app/src/main/java/com/tjeit/a20191222_01_loginandsignupapi/SignUpActivity.kt
+++ app/src/main/java/com/tjeit/a20191222_01_loginandsignupapi/SignUpActivity.kt
@@ -2,7 +2,10 @@ package com.tjeit.a20191222_01_loginandsignupapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.tjeit.a20191222_01_loginandsignupapi.utils.ConnectServer
+import kotlinx.android.synthetic.main.activity_sign_up.*
+import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
 
@@ -15,6 +18,26 @@ class SignUpActivity : BaseActivity() {
 
 
     override fun setupEvents() {
+
+        signUpBtn.setOnClickListener {
+
+//            사용자가 입력한 값을 별개로 저장
+            val inputId = idEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+            val inputName = nameEdt.text.toString()
+            val inputPhone = phoneEdt.text.toString()
+
+            ConnectServer.putRequestSignUp(mContext, inputId, inputPw, inputName, inputPhone, object : ConnectServer.JsonResponseHandler {
+                override fun onResponse(json: JSONObject) {
+
+                    Log.d("회원가입응답", json.toString())
+
+                }
+
+            })
+
+
+        }
 
     }
 
