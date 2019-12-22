3b3ff356d3d7bd3f5b426ad1bbac9b10fd0c3886
조경진 <kj_cho@idealidea.co.kr>
2019-12-22 16:10:00 +0900

    게시글 작성 액티비티 만들고 연결작업까지

---
+++ app/src/main/java/com/tjeit/a20191222_01_loginandsignupapi/EditBlackListActivity.kt
@@ -0,0 +1,23 @@
+package com.tjeit.a20191222_01_loginandsignupapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditBlackListActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_black_list)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
